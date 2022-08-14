90c1e693b2aa6ca31a46c522e6ec38dc18d07c87
Exidel <osimeyuko@gmail.com>
2022-08-14 16:05:31 +0300

    Added object with colors to keep all colors in one place

---
+++ src/main/kotlin/Colors.kt
@@ -0,0 +1,15 @@
+import androidx.compose.ui.graphics.Color
+
+
+object Colors {
+
+    val bg = Color(80, 80, 80, 255)
+    val itemBG = Color(1f, 1f, 1f, 1f)
+    val itemBorder = Color(0f, 0f, 0f, 1f)
+    val textColor = Color(0f, 0f, 0f, 1f)
+    val completeItemBG = Color(200, 200, 200, 255)
+    val completeTextColor = Color(50, 50, 50, 255)
+    val activeTask = Color(0, 150, 0, 255)
+    val completeTask = Color(150, 0, 0, 255)
+
+}
